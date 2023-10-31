2680e7dfb877b28ca57a88b61cd891cd392bf350
takahashi-pao <a.mapi.baseball@gmail.com>
2023-10-31 15:51:00 +0900

    未ログイン状態での登録画面遷移を拒否する制御（フロント）

--- react-app/src/components/register/Register.tsx
+++ react-app/src/components/register/Register.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import FileUploadComponent from './parts/register';
+import RegisterComponent from './parts/register';
 import { Link, NavigateFunction, useNavigate } from 'react-router-dom';
 
 type auth_response_props = {
@@ -28,6 +28,11 @@ function Register() {
       }
       // 取得したデータをstateに設定
       setIsLoggedIn(data.isSignIn);
+      console.log("fetch end")
+      // ログインしていない場合、ホーム画面へ遷移
+      if (!data.isSignIn) {      
+        navigate("/");
+      }
     } catch (error) {
       console.error('データを取得できませんでした:', error);
     }
@@ -36,15 +41,18 @@ function Register() {
   useEffect(() => {
     // コンポーネントがマウントされたときにログイン状態を取得
     fetchUserStatus();
-    if (!isLoggedIn) {      
-      navigate("/")
-    }
+    console.log("component mounted")
   }, []);
+
+  // ログイン状態に応じて遷移
+  useEffect(() => {
+    console.log("isLoginStatus changed")
+  }, [isLoggedIn]);
 
   return (
     <div>
       <Link to="/">Go To Home</Link>
-      <FileUploadComponent key="UploadComponent"></FileUploadComponent>
+      <RegisterComponent key="UploadComponent"></RegisterComponent>
     </div>
     
   );
