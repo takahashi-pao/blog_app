d0223d13c91b4772cd0f5b654a092743e7d51888
takahashi-pao <a.mapi.baseball@gmail.com>
2023-10-31 15:52:55 +0900

    ・未サインインユーザーの登録画面遷移拒否処理（サーバー） ・ミドルウェアのフォルダ区分、パッケージ整理

--- go/cmd/main.go
+++ go/cmd/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base32"
-	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
 
 	"example.com/blog_app/go/internal/handlers/article"
 	"example.com/blog_app/go/internal/handlers/auth"
@@ -15,17 +10,16 @@ import (
 	"example.com/blog_app/go/internal/handlers/thumbnail"
 	"example.com/blog_app/go/middleware"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/sessions"
 )
 
 func main() {
 	// Ginのルーターを作成
 	router := gin.Default()
 	// リクエスト元ポートの設定
-	router.Use(corsMiddleware())
+	router.Use(middleware.CorsMiddleware())
 
 	// セッションの設定
-	sessionInit()
+	middleware.SessionInit()
 	authGroup := router.Group("/auth")
 	authGroup.Use(middleware.AuthMiddleware())
 	{
@@ -63,66 +57,4 @@ func main() {
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func corsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// リクエスト元のオリジンを取得
-		origin := c.Request.Header.Get("Origin")
-
-		// ポート番号が3000であるオリジンのみ許可
-		if origin == "http://localhost:3000" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-
-		// 他の必要なCORSヘッダーを設定
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-		// OPTIONSリクエストに対応するために、OPTIONSメソッドの場合は早期に終了する
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		// 次のミドルウェアやハンドラを実行
-		c.Next()
-	}
-}
-
-// セッション用の初期処理
-func sessionInit() {
-
-	// 乱数生成
-	b := make([]byte, 48)
-	_, err := io.ReadFull(rand.Reader, b)
-	if err != nil {
-		panic(err)
-	}
-	str := strings.TrimRight(base32.StdEncoding.EncodeToString(b), "=")
-
-	// 新しいstoreとセッションを準備
-	auth.Store = sessions.NewCookieStore([]byte(str))
-	auth.Session = sessions.NewSession(auth.Store, auth.Session_name)
-
-	// セッションの有効範囲を指定
-	auth.Store.Options = &sessions.Options{
-		Domain:   "localhost",
-		Path:     "/",
-		MaxAge:   3600 * 24 * 7,
-		Secure:   false,
-		HttpOnly: true,
-	}
-
-	// log
-	fmt.Println("key     data --")
-	fmt.Println(str)
-	fmt.Println("")
-	fmt.Println("store   data --")
-	fmt.Println(auth.Store)
-	fmt.Println("")
-	fmt.Println("session data --")
-	fmt.Println(auth.Session)
-	fmt.Println("")
-
 }
--- go/middleware/middleware.go
+++ go/middleware/middleware.go
@@ -1,10 +1,15 @@
 package middleware
 
 import (
+	"crypto/rand"
+	"encoding/base32"
 	"fmt"
+	"io"
+	"strings"
 
 	"example.com/blog_app/go/internal/handlers/auth"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
@@ -17,4 +22,66 @@ func AuthMiddleware() gin.HandlerFunc {
 
 		c.Next()
 	}
+}
+
+func CorsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// リクエスト元のオリジンを取得
+		origin := c.Request.Header.Get("Origin")
+
+		// ポート番号が3000であるオリジンのみ許可
+		if origin == "http://localhost:3000" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		// 他の必要なCORSヘッダーを設定
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// OPTIONSリクエストに対応するために、OPTIONSメソッドの場合は早期に終了する
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		// 次のミドルウェアやハンドラを実行
+		c.Next()
+	}
+}
+
+// セッション用の初期処理
+func SessionInit() {
+
+	// 乱数生成
+	b := make([]byte, 48)
+	_, err := io.ReadFull(rand.Reader, b)
+	if err != nil {
+		panic(err)
+	}
+	str := strings.TrimRight(base32.StdEncoding.EncodeToString(b), "=")
+
+	// 新しいstoreとセッションを準備
+	auth.Store = sessions.NewCookieStore([]byte(str))
+	auth.Session = sessions.NewSession(auth.Store, auth.Session_name)
+
+	// セッションの有効範囲を指定
+	auth.Store.Options = &sessions.Options{
+		Domain:   "localhost",
+		Path:     "/",
+		MaxAge:   3600 * 24 * 7,
+		Secure:   false,
+		HttpOnly: true,
+	}
+
+	// log
+	fmt.Println("key     data --")
+	fmt.Println(str)
+	fmt.Println("")
+	fmt.Println("store   data --")
+	fmt.Println(auth.Store)
+	fmt.Println("")
+	fmt.Println("session data --")
+	fmt.Println(auth.Session)
+	fmt.Println("")
+
 }
